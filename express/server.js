f406d83adb8e7bb51161391be7557a21d8ba9443
OutlyingAce <regmisiddhant1@gmail.com>
2025-01-10 01:53:38 +0545

    third commit (no db)

--- express/server.js
+++ express/server.js
@@ -1,17 +1,14 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const tasksRoutes = require("./routes/tasks"); 
-
+const express = require('express');
 const app = express();
-const PORT = 3000;
+const path = require('path');
 
-app.use(express.json());
-app.use("/tasks", tasksRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
